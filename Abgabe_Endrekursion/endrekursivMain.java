1864d66ed4c61fd9944567b8fcc573ebc101df23
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2021-11-09 12:48:35 +0100

    Update endrekursivMain.java

--- Abgabe_Endrekursion/endrekursivMain.java
+++ Abgabe_Endrekursion/endrekursivMain.java
@@ -2,24 +2,42 @@ package Abgabe_Endrekursion;
 
 public class endrekursivMain {
     public static void main(String[] args) {
+
+        long timeSumRe1 = System.nanoTime();
+        int sumRe = sumRekursiv(21);
+        long timeSumRe2 = System.nanoTime();
+
+        long timeSumEndRe1 = System.nanoTime();
+        int sumEndRe = sumEndRekursiv(0, 21);
+        long timeSumEndRe2 = System.nanoTime();
+        
+        int fakRe = fakRekursiv(3);
+        int fakEndRe = fakEndRekursiv(1, 3);
+
+        int powfakRe = powfakRekursiv(5, 3);
+        int powfakEndRe = powfakEndRekursiv(5, 3, 1);
+
+        int sumFolgeRe = sumFolgeRekursiv(6);
+        int sumFolgeEndRe = sumFolgeEndRekursiv(0, 6);
+
         System.out.println("Summenfunktion");
-        System.out.println("--> Rekursiv: " + sumRekursiv(21));
-        System.out.println("--> Endrekursiv: " + sumEndRekursiv(0, 21));
+        System.out.println("--> Rekursiv: " + sumRe + " Zeit: " + (timeSumRe2 - timeSumRe1) + "ns");
+        System.out.println("--> Endrekursiv: " + sumEndRe + " Zeit: " + (timeSumEndRe2 - timeSumEndRe1) + "ns");
         System.out.println();
 
         System.out.println("Fakultätsfunktion");
-        System.out.println("--> Rekursiv: " + fakRekursiv(3));
-        System.out.println("--> Endrekursiv: " + fakEndRekursiv(1, 3));
+        System.out.println("--> Rekursiv: " + fakRe);
+        System.out.println("--> Endrekursiv: " + fakEndRe);
         System.out.println();
 
         System.out.println("Hoch-Funktion");
-        System.out.println("--> Rekursiv: " + powfakRekursiv(5, 3));
-        System.out.println("--> Endrekursiv: " + powfakEndRekursiv(5, 3, 1));
+        System.out.println("--> Rekursiv: " + powfakRe);
+        System.out.println("--> Endrekursiv: " + powfakEndRe);
         System.out.println();
 
         System.out.println("Zusatz-Folge");
-        System.out.println("--> Rekursiv: " + sumFolgeRekursiv(6));
-        System.out.println("--> Endrekursiv: " + sumFolgeEndRekursiv(0, 6));
+        System.out.println("--> Rekursiv: " + sumFolgeRe);
+        System.out.println("--> Endrekursiv: " + sumFolgeEndRe);
     }
 
     //rekursive Methoden
