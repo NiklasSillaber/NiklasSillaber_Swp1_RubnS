7ed79236eeb4052d7c54c697797ad2c575aa31fc
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2022-01-19 21:24:24 +0100

    Update main.java

--- Üben_Test2/main.java
+++ Üben_Test2/main.java
@@ -4,28 +4,22 @@ import java.util.Arrays;
 public class main {
 
     public static void main(String[] args) {
-        Integer[] arr = {1, 30, 4, 2, 5, 7, 1, 20, 3, 6, 90, 3, 6, 2, 78};
-        char[] car = "NiklasdafdsfaASDSADdsdsaeegrts".toLowerCase().toCharArray();
+        Integer[] arr = {1, 30, 4, 2, 5, 7, 1, 20, 3, 6, 90, 3, 6, 2, 78, 20, 3, 5, 34,54, 45, 5,2 ,3};
+        //char[] car = "NiklasdafdsfaASDSADdsdsaeegrts".toLowerCase().toCharArray();
 
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(car));
-        arr = insertionSort(arr);
-        
         System.out.println();
-        System.err.println();
-        String a = "sabo";
-        char[] e = a.toCharArray();
-        a=e.toString();
-        System.out.println(Arrays.toString(e));
-        System.out.println(a);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(car));
+        //System.out.println(Arrays.toString(car));
+        arr = bubbleSort(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + ", ");
+        }
+        
     }
 
-    public static Integer[] bubbleSort(Integer[] arr) {
-
-        for(int i = 0; i < arr.length; i ++){
-            for(int j = 0; j < arr.length - i - 1; j++){
+    public static Integer[] bubbleSort(Integer[] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length - i - 1; j++) {
                 if(arr[j] > arr[j + 1]){
                     int tmp = arr[j];
                     arr[j] = arr[j + 1];
@@ -38,37 +32,34 @@ public class main {
 
     public static Integer[] selectionSort(Integer[] arr){
         for(int i = 0; i < arr.length; i ++){
-            int minX = i;
+            int min = i;
             for(int j = i + 1; j < arr.length; j++){
-                if(arr[minX] > arr[j]){
-                    minX = j;
+                if(arr[j] < arr[min]){
+                    min = j;
                 }
             }
-            if(minX != i){
-                int tmp = arr[i];
-                arr[i] = arr[minX];
-                arr[minX] = tmp; 
+            int tmp;
+            if(min != i){
+                tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
             }
         }
         return arr;
     }
 
     public static Integer[] insertionSort(Integer[] arr){
-        int j;
-        int temp;
         for(int i = 0; i < arr.length; i++){
-            temp= arr[i];
-            j = i;
-            while(j > 0 && arr[j-1] > temp){
+            int tmp = arr[i];
+            int j = i;
+            while(j > 0 && arr[j - 1] > tmp){
                 arr[j] = arr[j - 1];
                 j--;
             }
-            arr[j] = temp;
+            arr[j] = tmp;
         }
         return arr;
     }
-
-    
 
     
 }
