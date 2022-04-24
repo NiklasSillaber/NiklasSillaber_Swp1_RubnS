a9ad5adccfedeaff61f07e3e01c578f98f4258c2
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2022-04-24 22:07:40 +0200

    Create main.java

---
+++ Üben_Test3/main.java
@@ -0,0 +1,25 @@
+package Üben_Test3;
+
+import javax.swing.event.ChangeEvent;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        
+        
+        change();
+        
+        System.out.print(s + i);
+
+    }
+
+    static String s = "hallo";
+    static int i = 1;
+
+    public static void change(){
+        s = "tschau";
+        i = 2;
+    }
+    
+}
